f64fee723335d2dcb768a132ef9dce81f510e706
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2026-01-17 21:00:58 +0530

    Left days updated

--- app.js
+++ app.js
@@ -658,7 +658,7 @@ const todayLeads = await Lead.findAll({
             count: count,
             todayCount: todayLeads.length,
             todayLeads: todayLeads,
-            left: 8
+            left: 9
         });
     } catch (error) {
         res.status(500).json({ 
