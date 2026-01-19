e1d25ee23e3c9e5f7fc2af7a1b6c04c563456d39
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2026-01-19 16:35:06 +0530

    Increment left value from 10 to 11

--- app.js
+++ app.js
@@ -658,7 +658,7 @@ const todayLeads = await Lead.findAll({
             count: count,
             todayCount: todayLeads.length,
             todayLeads: todayLeads,
-            left: 10
+            left: 11
         });
     } catch (error) {
         res.status(500).json({ 
