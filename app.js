2660462560a3cb2b0fc02a1b42d14536d764c681
Sammedpatil29 <108979063+Sammedpatil29@users.noreply.github.com>
2026-04-12 22:47:45 +0530

    Uncomment CORS middleware in app.js

--- app.js
+++ app.js
@@ -23,7 +23,8 @@ app.use(cors(
 //       callback(new Error('Not allowed by CORS'));
 //     }
 //   },
-// })
+// }
+       )
        );
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
