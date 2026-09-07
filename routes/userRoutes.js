57461351e011569e60a3eb40738ebb6237de4da2
Sammed <sammed.patil29@gmail.com>
2026-09-07 23:16:58 +0530

    manage user added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
-import { getAllUsers, createUser, updateFcmToken, verifyToken, login } from '../controllers/userController.js';
+import { getAllUsers, createUser, updateUser, deleteUser, updateFcmToken, verifyToken, login } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/users', getAllUsers);
 router.post('/users', createUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 router.patch('/users/fcm-token', updateFcmToken);
 router.post('/verify-token', verifyToken);
 router.post('/login', login);
